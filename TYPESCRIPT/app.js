7c1d0cee43b6cf17720fe4f03210a36509da1f1e
OscarMunozBernales <munoz.bernales.oscar@gmail.com>
2020-05-12 21:38:33 -0400

    desestructuracion en ts

--- TYPESCRIPT/app.js
+++ TYPESCRIPT/app.js
@@ -1,14 +1,35 @@
 "use strict";
 (function () {
-    function activar(quien, momento, objeto) {
-        if (objeto === void 0) { objeto = 'Batiseñal'; }
-        if (momento) {
-            console.log(quien + " activ\u00F3 la " + objeto + ", en la " + momento);
-        }
-        else {
-            console.log(quien + " activ\u00F3 la " + objeto);
-        }
-    }
-    activar('Joker');
-    activar('Gordon', 'batiseñal', 'tarde');
+    var avenger = {
+        nombre: 'Peter',
+        poder: 'Poderes aracnidos',
+        clave: 'Spiderman'
+    };
+    // DESESTRUCTURACION
+    var nombre = avenger.nombre, poder = avenger.poder, clave = avenger.clave;
+    console.log(nombre);
+    console.log(poder);
+    console.log(clave);
+    // DESESTRUCTURACION EN UNA FUNCION
+    var extraer = function (_a) {
+        var nombre = _a.nombre, clave = _a.clave;
+        console.log(nombre);
+        console.log(clave);
+    };
+    extraer(avenger);
+    // EXTRAER ELEMENTOS DESDE UN ARREGLO
+    var avengers = ['Thor', 'IronMan', 'SpiderMan'];
+    var martillo = avengers[0], sexboy = avengers[1], arana = avengers[2];
+    console.log(martillo);
+    console.log(sexboy);
+    console.log(arana);
+    var posicion3 = avengers[2]; // ESTO QUIERE DECIR QUE NO NOS INTERESA LA POS 1 Y 2 PERO SI LA 3
+    console.log(posicion3);
+    var extraerArr = function (_a) {
+        var thor = _a[0], ironman = _a[1], spiderman = _a[2];
+        console.log(thor);
+        console.log(ironman);
+        console.log(spiderman);
+    };
+    extraerArr(avengers);
 })();
--- TYPESCRIPT/app.ts
+++ TYPESCRIPT/app.ts
@@ -1,19 +1,49 @@
 (function(){
 
-    function activar( 
-            quien:string, 
-            momento?:string,
-            objeto:string = 'Batiseñal'){
+    const avenger = {
+        nombre  : 'Peter',
+        poder   : 'Poderes aracnidos',
+        clave   : 'Spiderman'
+    };
 
-        if( momento ){
-            console.log( `${ quien } activó la ${ objeto }, en la ${ momento }` );
-        }else{
-            console.log( `${ quien } activó la ${ objeto }` );
-        }
+    // DESESTRUCTURACION
 
+    const { nombre , poder, clave } = avenger;
+    console.log( nombre );
+    console.log( poder );
+    console.log( clave );
+
+    // DESESTRUCTURACION EN UNA FUNCION
+
+    const extraer = ( {nombre, clave} : any ) => {
+        console.log( nombre );
+        console.log( clave );
     }
 
-    activar('Joker');
-    activar('Gordon', 'batiseñal', 'tarde');
+    extraer( avenger );
+
+    // EXTRAER ELEMENTOS DESDE UN ARREGLO
+
+    const avengers:string[] = ['Thor', 'IronMan', 'SpiderMan'];
+
+    const [ martillo, sexboy, arana ] = avengers;
+
+    console.log( martillo );
+    console.log( sexboy );
+    console.log( arana );
+
+    const [ , , posicion3 ] = avengers; // ESTO QUIERE DECIR QUE NO NOS INTERESA LA POS 1 Y 2 PERO SI LA 3
+
+    console.log( posicion3 );
+
+    const extraerArr = ( [ thor, ironman, spiderman ]: string[] ) => {
+        console.log( thor );
+        console.log( ironman );
+        console.log( spiderman );
+    }
+
+    extraerArr( avengers );
+
+
 
 })();
