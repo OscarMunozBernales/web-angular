785364efb8aa97e54b233c39d0b748f42b2401a3
OscarMunozBernales <munoz.bernales.oscar@gmail.com>
2020-05-12 21:09:44 -0400

    template literario

--- TYPESCRIPT/app.js
+++ TYPESCRIPT/app.js
@@ -1,3 +1,12 @@
 "use strict";
 (function () {
+    var sumar = function (numero1, numero2) {
+        return numero1 + numero2;
+    };
+    var nombre = 'Oscar';
+    var apellido = 'Muñoz';
+    var edad = 28;
+    var salida = nombre + " " + apellido + ", " + edad + " a\u00F1os de edad";
+    console.log(salida);
+    console.log("" + sumar(30, 12));
 })();
--- TYPESCRIPT/app.ts
+++ TYPESCRIPT/app.ts
@@ -1,4 +1,16 @@
 (function(){
 
-    
+    const sumar = ( numero1:number, numero2:number ) => {
+        return numero1 + numero2;
+    }
+
+    const nombre    = 'Oscar';
+    const apellido  = 'Muñoz';
+    const edad      = 28;
+
+    const salida = `${ nombre } ${ apellido }, ${ edad } años de edad`;
+
+    console.log( salida );
+    console.log( `${ sumar(30, 12) }`);
+
 })();
