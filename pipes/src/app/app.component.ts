7bf6e91aac56421b5e305eb7db45c5ecb85ddbd6
OscarMunozBernales <munoz.bernales.oscar@gmail.com>
2020-05-29 14:30:08 -0400

    pipe decimal

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -8,6 +8,5 @@ import { Component } from '@angular/core';
 export class AppComponent {
   nombre: string = 'Oscar Muñoz Bernales';
   numeroArr: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-
+  PI: number = Math.PI;
 }
