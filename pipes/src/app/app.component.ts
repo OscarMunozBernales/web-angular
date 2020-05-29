3abb57c185c1d1269c84e67b4064420f52858544
OscarMunozBernales <munoz.bernales.oscar@gmail.com>
2020-05-29 14:26:30 -0400

    pipe slice

--- pipes/src/app/app.component.ts
+++ pipes/src/app/app.component.ts
@@ -7,6 +7,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   nombre: string = 'Oscar Muñoz Bernales';
+  numeroArr: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 
 }
