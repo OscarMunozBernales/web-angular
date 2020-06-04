2966880ed1c546d4772875508820cb82384af9f8
OscarMunozBernales <munoz.bernales.oscar@gmail.com>
2020-06-03 22:13:53 -0400

    optimizacion codigo

--- spotiApp/src/app/services/spotify.service.ts
+++ spotiApp/src/app/services/spotify.service.ts
@@ -7,25 +7,28 @@ import { map } from 'rxjs/operators';
 })
 export class SpotifyService {
 
+  
+
   constructor( private http: HttpClient) {
     // console.log( 'Spotify s  ervices listo!' );
   }
 
-  getNewReleases(){
+  getQuery( query: string){
+    const URL = `https://api.spotify.com/v1/${ query }`;
 
     const headers = new HttpHeaders({
       Authorization: 'Bearer BQCD6YYqngZcgZ0gfyeShUzrOcHdSAJCxFWHFt2ftEfHFzUZ2tYZTEZxph9zyn1RFn6MOS2UsPfNaRL4yd4'
     });
 
-    return this.http.get('https://api.spotify.com/v1/browse/new-releases?limit=50', { headers }).pipe( map( data => data['albums'].items));
+    return this.http.get(URL, {headers});
+  }
 
+  getNewReleases(){
+
+    return this.getQuery( 'browse/new-releases?limit=20' ).pipe( map( data => data['albums'].items));
   }
 
   getArtista( termino: string ){
-    const headers = new HttpHeaders({
-      Authorization: 'Bearer BQCD6YYqngZcgZ0gfyeShUzrOcHdSAJCxFWHFt2ftEfHFzUZ2tYZTEZxph9zyn1RFn6MOS2UsPfNaRL4yd4'
-    });
-
-    return this.http.get(`https://api.spotify.com/v1/search?query=${ termino }&type=artist&offset=0&limit=20`, { headers }).pipe( map( data => data['artists'].items));
+    return this.getQuery( `search?query=${ termino }&type=artist&offset=0&limit=20` ).pipe( map( data => data['artists'].items));
   }
 }
