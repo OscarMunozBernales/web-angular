465e415f372643f8ec9f830a497f77a5ed81b6cc
OscarMunozBernales <munoz.bernales.oscar@gmail.com>
2020-06-22 19:42:18 -0400

    Validaciones síncronas

--- 7-formularios/src/app/pages/reactive/reactive.component.ts
+++ 7-formularios/src/app/pages/reactive/reactive.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormBuilder } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive',
@@ -20,9 +20,27 @@ export class ReactiveComponent implements OnInit {
   crearFormulario(){
 
     this.forma = this.formBuilder.group({
-      nombre  : ['Oscar'],
-      apellido: ['Muñoz'],
-      correo  : ['asd@asd.cl'],
+      nombre  : [
+        '',  // VALOR POR DEFECTO
+        [ // ARRAY DE VALIDACIONES DEL formBuilder
+          Validators.required, 
+          Validators.minLength(5)
+        ] 
+      ],
+      apellido: [
+        '', 
+        [
+          Validators.required,
+          Validators.minLength( 5 )
+        ]
+      ],
+      correo  : [
+        '', 
+        [
+          Validators.required,
+          Validators.pattern( '[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$' )
+        ] 
+      ],
     });
 
   }
