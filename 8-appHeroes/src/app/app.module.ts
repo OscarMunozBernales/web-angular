b880854b8207c73646f95f6c296bad2c81fde6f4
OscarMunozBernales <munoz.bernales.oscar@gmail.com>
2020-06-23 13:30:05 -0400

    agregando librerias bootstrap, font awesom y animate.css

--- 8-appHeroes/src/app/app.module.ts
+++ 8-appHeroes/src/app/app.module.ts
@@ -1,12 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// RUTAS
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { HeroesComponent } from './pages/heroes/heroes.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeroesComponent,
+    HeroeComponent
   ],
   imports: [
     BrowserModule,
